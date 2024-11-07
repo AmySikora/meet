4ebc502da16210605d45d17bd6bad49821deebc9
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-07 09:27:59 -0800

    updated handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://amysikora.github.io/meet/"
+  "https://jerrintm.github.io/meet/"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -15,41 +15,11 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 
 module.exports.getAuthURL = async () => {
-   // Decode authorization code extracted from the URL query
-   const code = decodeURIComponent(`${event.pathParameters.code}`);
-
-   return new Promise((resolve, reject) => {
-  /**  Exchange authorization code for access token with a “callback” after the exchange,
-  *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
-  */
-
-  oAuth2Client.getToken(code, (error, response) => {
-    if (error) {
-      return reject(error);
-    }
-    return resolve(response);
-  });
-})
-  .then((results) => {
-    // Respond with OAuth token 
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-      body: JSON.stringify(results),
-    };
-  })
-  .catch((error) => {
-    // Handle error
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error),
-    };
-  });
-};
-
+  /**
+   *
+   * Scopes array is passed to the `scope` option. 
+   *
+   */
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -65,4 +35,45 @@ module.exports.getAuthURL = async () => {
       authUrl,
     }),
   };
+};
 
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
+
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(error),
+      };
+    });
+}
