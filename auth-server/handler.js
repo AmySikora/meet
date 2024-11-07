3bef4b8a37e7bd02245d6f75e9217b5776630f4b
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-07 11:02:16 -0800

    updated typo

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,8 +5,9 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://https://amysikora.github.io/meet/"
+  "https://amysikora.github.io/meet/" 
 ];
+
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
