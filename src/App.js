ee1b0e21f0afc3319f47b6222fe3b4b73313f4f6
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-09 12:55:06 -0800

    Updated app.js file for testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to My Meet App!</h1>
-        <p>
-         <code>src/App.js</code> Welcome to My Meet App!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
