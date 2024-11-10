83201bc8d8d9c9546ebc084a0d217c7e0ce6f720
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-09 20:58:23 -0800

    updated city search

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,15 +1,17 @@
 // src/components/CitySearch.js
 
+import { useState } from "react";
+
 const CitySearch = () => {
-    return (
-      <div id="city-search">
-        <input
-          type="text"
-          className="city"
-                  placeholder="Search for a city"
-        />
-      </div>
-    )
-   }
-  
-  export default CitySearch;
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  return (
+    <div id="city-search">
+      <input
+        type="text"
+        className="city"
+      />
+    </div>
+  )
+}
+
+export default CitySearch;
