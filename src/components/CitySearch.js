9d61be994b6fe06a34682fbbb71a9cd0ed6abf12
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-09 20:52:18 -0800

    updating to fix error

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,20 +1,15 @@
 // src/components/CitySearch.js
 
-import { useState } from "react";
-
 const CitySearch = () => {
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  return (
-    <div id="city-search">
-      <input
-        type="text"
-        className="city"
-                placeholder="Search for a city"
-        onFocus={() => setShowSuggestions(true)}
-      />
-      {showSuggestions ? <ul className="suggestions"></ul> : null}
-    </div>
-  )
-}
-
-export default CitySearch;
+    return (
+      <div id="city-search">
+        <input
+          type="text"
+          className="city"
+                  placeholder="Search for a city"
+        />
+      </div>
+    )
+   }
+  
+  export default CitySearch;
