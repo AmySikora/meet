f4e2dd27cdb60fe16b9c1e0ee7be8f13665f1962
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-09 19:55:51 -0800

    added a placeholder

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -9,7 +9,9 @@ const CitySearch = () => {
       <input
         type="text"
         className="city"
+            placehollder="Search for a city"
       />
+      {showSuggestions ? <ul className="suggestions"></ul> : null}
     </div>
   )
 }
