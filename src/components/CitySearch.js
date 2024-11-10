97bdb224ed8e217c66b2b89226987e5ae4596cf0
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-09 23:17:34 -0800

    updated city search

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -11,38 +11,36 @@ const CitySearch = ({ allLocations }) => {
     const value = event.target.value;
     const filteredLocations = allLocations ? allLocations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    }) : []; 
+    }) : [];
+
     setQuery(value);
     setSuggestions(filteredLocations);
-    };
+  };
 
   return (
-
-        <div id="city-search">
-          <input 
-            type="text"
-            className="city"
-            placeholder="Search for a city"
-            value={query}
-            onFocus={() => setShowSuggestions(true)}
-            onChange={handleInputChanged}
-          />
-          {showSuggestions ? 
-            <ul className="suggestions">
-              {suggestions.map((suggestion) => {
-                return <li 
-                          onClick={handleItemClicked} 
-                          key={suggestion}>{suggestion}
-                        </li>
-              })}
-              <li key='See all cities'
-                  onClick={handleItemClicked}
-              >
-                <b>See all cities</b>
-              </li>
-            </ul> : null}
-        </div>      
-      )
-  }
-     
+    <div id="city-search">
+      <input 
+        type="text"
+        className="city"
+        placeholder="Search for a city"
+        value={query}
+        onFocus={() => setShowSuggestions(true)}
+        onChange={handleInputChanged}
+        />
+        {showSuggestions ? 
+        <ul className="suggestions">
+          {suggestions.map((suggestion) => {
+            return <li key={suggestion}>{suggestion}</li>
+          })}
+          <li key='See all cities'>
+            <b>See all cities</b>
+          </li>
+        </ul>
+        : null
+      }
+    
+    </div>      
+  )
+}
+ 
 export default CitySearch;
