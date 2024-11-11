b978739f8aa01449243be8bdf4585423ccf272c4
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-11 14:50:36 -0800

    updated city search

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,44 +1,15 @@
 // src/components/CitySearch.js
 
-import { useState } from "react";
-
-const CitySearch = ({allLocations, setCurrentCity, setInfoAlert}) => {
-    const [showSuggestions, setShowSuggestions] = useState(false);
-    const [query, setQuery] = useState("");
-    const [suggestions, setSuggestions] = useState([]);
-    
-    const handleInputChanged = (event) => {
-      const value = event.target.value;
-      const filteredLocations = allLocations ? allLocations.filter((location) => {
-        return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-      }) : [];
-  
-      setQuery(value);
-      setSuggestions(filteredLocations);
-  
-      return (
-        <div id="city-search">
-          <input
-            type="text"
-            className="city"
-            placeholder="Search for a city"
-            value={query}
-            onFocus={() => setShowSuggestions(true)}
-            onChange={handleInputChanged}
-          />
-          {showSuggestions ?
-            <ul className="suggestions">
-              {suggestions.map((suggestion) => {
-                return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
-              })}
-              <li key='See all cities' onClick={handleItemClicked}>
-                <b>See all cities</b>
-              </li>
-            </ul>
-            : null
-          }
-      </div>      
+const CitySearch = () => {
+    return (
+      <div id="city-search">
+        <input
+          type="text"
+          className="city"
+                  placeholder="Search for a city"
+        />
+      </div>
     )
-}
-}   
-export default CitySearch;
+   }
+  
+  export default CitySearch;
