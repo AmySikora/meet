d1dbb8ce92b580efe33ae43d1f64dd7c3189bc46
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-16 13:41:57 -0800

    updated fileterEventsByCity.test.js

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,8 +1,8 @@
+import { render, waitFor, within } from '@testing-library/react';
 import { loadFeature, defineFeature } from 'jest-cucumber';
-import { render, within, waitFor } from '@testing-library/react';
 import App from '../App';
-import { getEvents } from '../mock-data';
 import userEvent from '@testing-library/user-event';
+import { getEvents } from '../api';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
@@ -71,17 +71,25 @@ defineFeature(feature, test => {
             expect(suggestionListItems).toHaveLength(2);
         });
 
-        when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
+        when('the user selects a city (e.g., “Berlin, Germany”) from the list', async () => {
+            const user = userEvent.setup();
+            await user.click(suggestionListItems[1]);
+          });
 
-        });
+          then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
+            expect(citySearchInput.value).toBe('Berlin, Germany');
+          });
 
-        then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-
-        });
-
-        and('the user should receive a list of upcoming events in that city', () => {
-
-        });
+          and('the user should receive a list of upcoming events in that city', async () => {
+            const EventListDOM = AppDOM.querySelector('#event-list');
+            const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+            const allEvents = await getEvents();
+      
+            // filtering the list of all events down to events located in Germany
+            // citySearchInput.value should have the value "Berlin, Germany" at this point
+            const berlinEvents = allEvents.filter(event => event.location === citySearchInput.value)
+            expect(EventListItems).toHaveLength(berlinEvents.length);
+          });
     });
 
 });
