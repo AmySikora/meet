992c0af78c9e0b1120a74758d63b94e2f2d721cc
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-26 22:06:52 -0800

    Updated api.js for offline data storage

--- src/api.js
+++ src/api.js
@@ -16,7 +16,75 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-const removeQuery = () => {
+const checkToken = async (accessToken) => {
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
+};
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+      if (!code) {
+        const response = await fetch(
+          "https://mt7azmlov5.execute-api.us-west-2.amazonaws.com/dev/api/get-auth-url"
+        );
+        const result = await response.json();
+        const { authUrl } = result;
+        return (window.location.href = authUrl);
+      }
+      return code && getToken(code);
+      }
+  return accessToken;
+}
+
+    const getToken = async (code) => {
+      const encodeCode = encodeURIComponent(code);
+      const response = await fetch(
+        'https://mt7azmlov5.execute-api.us-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode
+      );
+      const { access_token } = await response.json();
+      access_token && localStorage.setItem("access_token", access_token);
+
+      return access_token;
+    };
+/**
+ *
+ * This function will fetch the list of all events
+ */
+
+export const getEvents = async () => {
+    if (window.location.href.startsWith("http://localhost")) {
+      return mockData;
+    }
+    if (!navigator.onLine) {
+      const events = localStorage.getItem("lastEvents");
+      NProgress.done();
+      return events?JSON.parse(events):[];
+    }
+    const token = await getAccessToken();
+
+    if (token) {
+      removeQuery();
+      const url = `https://mt7azmlov5.execute-api.us-west-2.amazonaws.com/dev/api/get-events/${token}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      if (result) {
+        NProgress.done();
+        localStorage.setItem("lastEvents", JSON.stringify(result.events));
+        return result.events;
+      } else return null;
+    }
+  };
+
+  const removeQuery = () => {
     let newurl;
     if (window.history.pushState && window.location.pathname) {
       newurl =
@@ -30,77 +98,3 @@ const removeQuery = () => {
       window.history.pushState("", "", newurl);
     }
   };
-  
-  const checkToken = async (accessToken) => {
-    const response = await fetch(
-      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-    );
-    const result = await response.json();
-    return result;
-  };
-
-/**
- *
- * This function will fetch the list of all events
- */
-
-export const getEvents = async () => {
-  try {
-    if (window.location.href.startsWith("http://localhost")) {
-      return mockData;
-    }
-
-    const token = await getAccessToken();
-    if (token) {
-      removeQuery();
-      const url = `https://mt7azmlov5.execute-api.us-west-2.amazonaws.com/dev/api/get-events/${token}`;
-      console.log("Fetching events from URL:", url);
-
-      const response = await fetch(url);
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const result = await response.json();
-      console.log("Fetched events:", result.events);
-      return result.events || null;
-    }
-  } catch (error) {
-    console.error("Error in getEvents:", error);
-    throw error; 
-  }
-};
-
-  const getToken = async (code) => {
-    const encodeCode = encodeURIComponent(code);
-    const response = await fetch(
-      'https://mt7azmlov5.execute-api.us-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode
-    );
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
-  
-    return access_token;
-  };
-
-  export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-  
-    if (!accessToken || tokenCheck.error) {
-      await localStorage.removeItem("access_token");
-      const searchParams = new URLSearchParams(window.location.search);
-      const code = await searchParams.get("code");
-        if (!code) {
-          const response = await fetch(
-            "https://mt7azmlov5.execute-api.us-west-2.amazonaws.com/dev/api/get-auth-url"
-          );
-          const result = await response.json();
-          const { authUrl } = result;
-          return (window.location.href = authUrl);
-        }
-        return code && getToken(code);
-        }
-    return accessToken;
-  }
-
