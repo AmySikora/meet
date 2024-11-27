a7715046b3b0294398d462b5128554ffd69cb7bb
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-27 14:22:02 -0800

    removed callback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import EventList from "./components/EventList";
 import CitySearch from "./components/CitySearch";
